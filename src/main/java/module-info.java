b2187b86a7c026dd8dac2ef531cdbe3707a2c691
Sitjn64 <Stijnlammers2005@gmail.com>
2025-04-28 12:15:29 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.example.voxelizer {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.voxelizer to javafx.fxml;
+    exports com.example.voxelizer;
+}
